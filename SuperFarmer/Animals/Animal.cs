f62e6d290d1cfacdb45ce1738a315c4e015f4137
lursz <lukoszr@gmail.com>
2024-05-13 22:20:33 +0200

    Project blueprint

---
+++ SuperFarmer/Animals/Animal.cs
@@ -0,0 +1,6 @@
+namespace SuperFarmer.Animals;
+
+public interface Animal
+{
+    
+}
---
+++ SuperFarmer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SuperFarmer;
+
+static class Program
+{
+    public static void Main(string[] args)
+    {
+        
+    }
+}
+
